dd7d76d579e863b04f1e6e2757172683fd57dde0
Tyrone Chan <tyrone_chan@cathaypacific.com>
2020-06-22 00:38:58 +0800

    add mocks

---
+++ src/main/resources/db/data.sql
@@ -0,0 +1,5 @@
+insert into message values('1',747.1,1000.0,'EUR','GBP','FR',0.7471,'2018-01-24 10:27:44',134256);
+insert into message values('2',747.1,1000.0,'EUR','GBP','FR',0.7471,'2018-01-24 10:27:45',134256);
+insert into message values('3',747.1,1000.0,'EUR','GBP','US',0.7471,'2018-02-24 10:27:46',134229);
+insert into message values('4',747.1,1000.0,'EUR','GBP','US',0.7471,'2018-02-24 10:27:47',134229);
+insert into message values('5',747.1,1000.0,'EUR','GBP','HK',0.7471,'2018-03-24 10:27:48',134116);
